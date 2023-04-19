c0f42088a546151b3f7a7c77e5c7ce94f619e647
Joseignlave1 <jose.lavecchia@correo.ucu.edu.uy>
2023-04-18 21:45:20 -0300

    Realizé el ejercicio1

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -6,12 +6,14 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Full_GRASP_And_SOLID.Library
-{
+
+ {
     public class Recipe
     {
-        private ArrayList steps = new ArrayList();
+        private List<Step> steps = new List<Step>();
 
         public Product FinalProduct { get; set; }
 
@@ -34,5 +36,46 @@ namespace Full_GRASP_And_SOLID.Library
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
         }
-    }
+    
+        public double SumatoriaPrecioUnitario()
+        {
+            double Costoinsumos = 0.0;  
+            foreach (Step step in this.steps)
+            {
+                Costoinsumos+= step.Input.UnitCost*step.Quantity;
+            }
+            return Costoinsumos;
+        }
+
+
+            public double SumatoriaUsoCostoporhoraequipo()
+            {
+                double Costoequipamiento = 0.0;
+                foreach(Step step in this.steps)
+                {
+                    Costoequipamiento+= step.Equipment.HourlyCost*(step.Time/60.0);//divido por 60 para pasar a horas.
+
+                }
+                return Costoequipamiento;
+            }
+
+                
+            public double CalcularPrecioTotal()
+            {
+                double Costoinsumos=SumatoriaPrecioUnitario();
+                double Costoequipamiento=SumatoriaUsoCostoporhoraequipo();
+                double SumaTotal=Costoinsumos+Costoequipamiento;
+                return SumaTotal;
+            }
+
+            public double GetCalcularPrecioTotal()
+            {
+                return this.CalcularPrecioTotal();
+            }
 }
+// le asigné la responsabilidad de calcular el costo total a la clase recipe siguiendo el patrón expert que dice que una clase debe tener la responsabilidad de una tarea(en este caso calcular el costo total)
+// si es experta o posee el conocimiento para llevarla a cabo de manera correcta, lo que sucede en este caso con la clase recipe
+// ya que la clase Recipe tiene acceso a los métodos de Step y a la información de Recipe, teniendo esta información
+// posee todo el conocimiento para llevar a cabo la tarea de calcular el precio total.
+    
+ }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -26,6 +26,9 @@ namespace Full_GRASP_And_SOLID
             recipe.AddStep(new Step(GetProduct("Café"), 100, GetEquipment("Cafetera"), 120));
             recipe.AddStep(new Step(GetProduct("Leche"), 200, GetEquipment("Hervidor"), 60));
             recipe.PrintRecipe();
+            double PrecioTotal=recipe.CalcularPrecioTotal();
+            Console.WriteLine($"El precio total de su receta fue: {PrecioTotal}");
+            
         }
 
         private static void PopulateCatalogs()
